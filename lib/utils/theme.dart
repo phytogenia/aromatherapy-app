f2b0a80bf1f72ea39c4bd98ad62178e7c0a4ea67
Youcef Megoura <meg.youcef@yahoo.fr>
2022-03-01 20:10:59 +0100

    fix design theme inputs/ colors

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -5,29 +5,23 @@ import 'constants.dart';
 
 ThemeData theme() {
   return ThemeData(
-    scaffoldBackgroundColor: kSecondaryBackgroundColor,
-    appBarTheme: appBarTheme(),
-    textTheme: textTheme(),
-    inputDecorationTheme: inputDecorationTheme(),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
+      scaffoldBackgroundColor: kSecondaryBackgroundColor,
+      appBarTheme: appBarTheme(),
+      textTheme: textTheme(),
+      inputDecorationTheme: inputDecorationTheme(),
+      // visualDensity: VisualDensity.adaptivePlatformDensity,
+      primaryColor: kPrimaryColor,
+      textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: kPrimaryColor, selectionColor: kPrimaryColor));
 }
 
 InputDecorationTheme inputDecorationTheme() {
-  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(28),
-    borderSide: BorderSide(color: kPrimaryTextColor),
-    gapPadding: 10,
-  );
-  return InputDecorationTheme(
-    // If  you are using latest version of flutter then lable text and hint text shown like this
-    // if you r using flutter less then 1.20.* then maybe this is not working properly
-    // if we are define our floatingLabelBehavior in our theme then it's not applayed
-    floatingLabelBehavior: FloatingLabelBehavior.always,
-    contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
-    enabledBorder: outlineInputBorder,
-    focusedBorder: outlineInputBorder,
-    border: outlineInputBorder,
+  return const InputDecorationTheme(
+    floatingLabelStyle: TextStyle(color: kPrimaryColor),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: kPrimaryColor),
+    ),
+    iconColor: kPrimaryColor,
   );
 }
 
